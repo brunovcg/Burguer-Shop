eddfe50fd7cafebf0064ff55290cd07485b1cb95
brunogouveia <brunovcg@gmail.com>
2021-05-04 09:10:39 -0300

    alterado as keys para evitar erros

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,12 +6,12 @@ class Products extends Component {
     render(){
         return(
             this.props.produtos.map(x=>
-                <div className={this.props.giveClass}>
-                    <div className = "prodName">{x.name}</div>
-                    <div className = "prodCategory">{x.category}</div>
-                    <div className = "prodPrice">{`R$${(x.price).toFixed(2)}`}</div>
+                <ul className={this.props.giveClass} key={x.id}>
+                    <li className = "prodName">{x.name}</li>
+                    <li className = "prodCategory">{x.category}</li>
+                    <li className = "prodPrice">{`R$${(x.price).toFixed(2)}`}</li>
                     {this.props.showButton === "yes" ? <button className = "prodButton" onClick={()=> this.props.prodId(x.id)}>Adicionar</button> : <></>}
-                </div>
+                </ul>
                 
                 )
         )
